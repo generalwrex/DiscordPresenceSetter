a47d93efe9daa05882f5ab82d35ada82591f735e
Generalwrex <bjdroot@gmail.com>
2017-11-26 01:27:32 -0800

    Initial Commit

---
+++ DiscordPresenceSetter/Form1.Designer.cs
@@ -0,0 +1,406 @@
+﻿namespace DiscordPresenceSetter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.FormLog = new System.Windows.Forms.TextBox();
+            this.LargeImageKeyBox = new System.Windows.Forms.TextBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.LargeImageTextBox = new System.Windows.Forms.TextBox();
+            this.label13 = new System.Windows.Forms.Label();
+            this.MyApplicationID = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.DetailsBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SpectateSecretBox = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.JoinSecretBox = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.CurrentPartySize = new System.Windows.Forms.NumericUpDown();
+            this.MatchSecretBox = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.MaxPartySize = new System.Windows.Forms.NumericUpDown();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SmallImageKeyBox = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.StopButton = new System.Windows.Forms.Button();
+            this.StartButton = new System.Windows.Forms.Button();
+            this.PartyIDBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SmallImageTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.StateBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.CurrentPartySize)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.MaxPartySize)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.FormLog);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.LargeImageKeyBox);
+            this.splitContainer1.Panel2.Controls.Add(this.label12);
+            this.splitContainer1.Panel2.Controls.Add(this.LargeImageTextBox);
+            this.splitContainer1.Panel2.Controls.Add(this.label13);
+            this.splitContainer1.Panel2.Controls.Add(this.MyApplicationID);
+            this.splitContainer1.Panel2.Controls.Add(this.label11);
+            this.splitContainer1.Panel2.Controls.Add(this.DetailsBox);
+            this.splitContainer1.Panel2.Controls.Add(this.label2);
+            this.splitContainer1.Panel2.Controls.Add(this.SpectateSecretBox);
+            this.splitContainer1.Panel2.Controls.Add(this.label10);
+            this.splitContainer1.Panel2.Controls.Add(this.JoinSecretBox);
+            this.splitContainer1.Panel2.Controls.Add(this.label9);
+            this.splitContainer1.Panel2.Controls.Add(this.CurrentPartySize);
+            this.splitContainer1.Panel2.Controls.Add(this.MatchSecretBox);
+            this.splitContainer1.Panel2.Controls.Add(this.label8);
+            this.splitContainer1.Panel2.Controls.Add(this.label7);
+            this.splitContainer1.Panel2.Controls.Add(this.MaxPartySize);
+            this.splitContainer1.Panel2.Controls.Add(this.label6);
+            this.splitContainer1.Panel2.Controls.Add(this.SmallImageKeyBox);
+            this.splitContainer1.Panel2.Controls.Add(this.label5);
+            this.splitContainer1.Panel2.Controls.Add(this.StopButton);
+            this.splitContainer1.Panel2.Controls.Add(this.StartButton);
+            this.splitContainer1.Panel2.Controls.Add(this.PartyIDBox);
+            this.splitContainer1.Panel2.Controls.Add(this.label4);
+            this.splitContainer1.Panel2.Controls.Add(this.SmallImageTextBox);
+            this.splitContainer1.Panel2.Controls.Add(this.label3);
+            this.splitContainer1.Panel2.Controls.Add(this.StateBox);
+            this.splitContainer1.Panel2.Controls.Add(this.label1);
+            this.splitContainer1.Size = new System.Drawing.Size(690, 440);
+            this.splitContainer1.SplitterDistance = 319;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // FormLog
+            // 
+            this.FormLog.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.FormLog.Location = new System.Drawing.Point(0, 0);
+            this.FormLog.Multiline = true;
+            this.FormLog.Name = "FormLog";
+            this.FormLog.ReadOnly = true;
+            this.FormLog.Size = new System.Drawing.Size(319, 440);
+            this.FormLog.TabIndex = 0;
+            // 
+            // LargeImageKeyBox
+            // 
+            this.LargeImageKeyBox.Location = new System.Drawing.Point(121, 216);
+            this.LargeImageKeyBox.Name = "LargeImageKeyBox";
+            this.LargeImageKeyBox.Size = new System.Drawing.Size(227, 20);
+            this.LargeImageKeyBox.TabIndex = 50;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(23, 219);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(87, 13);
+            this.label12.TabIndex = 49;
+            this.label12.Text = "Large Image Key";
+            // 
+            // LargeImageTextBox
+            // 
+            this.LargeImageTextBox.Location = new System.Drawing.Point(121, 240);
+            this.LargeImageTextBox.Name = "LargeImageTextBox";
+            this.LargeImageTextBox.Size = new System.Drawing.Size(227, 20);
+            this.LargeImageTextBox.TabIndex = 48;
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(23, 243);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(90, 13);
+            this.label13.TabIndex = 47;
+            this.label13.Text = "Large Image Text";
+            // 
+            // MyApplicationID
+            // 
+            this.MyApplicationID.Location = new System.Drawing.Point(123, 16);
+            this.MyApplicationID.Name = "MyApplicationID";
+            this.MyApplicationID.Size = new System.Drawing.Size(227, 20);
+            this.MyApplicationID.TabIndex = 46;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(24, 19);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(70, 13);
+            this.label11.TabIndex = 45;
+            this.label11.Text = "ApplicationID";
+            // 
+            // DetailsBox
+            // 
+            this.DetailsBox.Location = new System.Drawing.Point(26, 90);
+            this.DetailsBox.Multiline = true;
+            this.DetailsBox.Name = "DetailsBox";
+            this.DetailsBox.Size = new System.Drawing.Size(322, 73);
+            this.DetailsBox.TabIndex = 44;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 43;
+            this.label2.Text = "Details";
+            // 
+            // SpectateSecretBox
+            // 
+            this.SpectateSecretBox.Location = new System.Drawing.Point(122, 346);
+            this.SpectateSecretBox.Name = "SpectateSecretBox";
+            this.SpectateSecretBox.Size = new System.Drawing.Size(227, 20);
+            this.SpectateSecretBox.TabIndex = 42;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(24, 349);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(84, 13);
+            this.label10.TabIndex = 41;
+            this.label10.Text = "Spectate Secret";
+            // 
+            // JoinSecretBox
+            // 
+            this.JoinSecretBox.Location = new System.Drawing.Point(121, 320);
+            this.JoinSecretBox.Name = "JoinSecretBox";
+            this.JoinSecretBox.Size = new System.Drawing.Size(227, 20);
+            this.JoinSecretBox.TabIndex = 40;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(23, 323);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(60, 13);
+            this.label9.TabIndex = 39;
+            this.label9.Text = "Join Secret";
+            // 
+            // CurrentPartySize
+            // 
+            this.CurrentPartySize.Location = new System.Drawing.Point(121, 378);
+            this.CurrentPartySize.Name = "CurrentPartySize";
+            this.CurrentPartySize.Size = new System.Drawing.Size(52, 20);
+            this.CurrentPartySize.TabIndex = 38;
+            // 
+            // MatchSecretBox
+            // 
+            this.MatchSecretBox.Location = new System.Drawing.Point(121, 292);
+            this.MatchSecretBox.Name = "MatchSecretBox";
+            this.MatchSecretBox.Size = new System.Drawing.Size(227, 20);
+            this.MatchSecretBox.TabIndex = 37;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(23, 295);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(71, 13);
+            this.label8.TabIndex = 36;
+            this.label8.Text = "Match Secret";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(199, 380);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(77, 13);
+            this.label7.TabIndex = 35;
+            this.label7.Text = "Max Party Size";
+            // 
+            // MaxPartySize
+            // 
+            this.MaxPartySize.Location = new System.Drawing.Point(297, 378);
+            this.MaxPartySize.Name = "MaxPartySize";
+            this.MaxPartySize.Size = new System.Drawing.Size(52, 20);
+            this.MaxPartySize.TabIndex = 34;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(23, 380);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(88, 13);
+            this.label6.TabIndex = 33;
+            this.label6.Text = "CurrentParty Size";
+            // 
+            // SmallImageKeyBox
+            // 
+            this.SmallImageKeyBox.Location = new System.Drawing.Point(121, 169);
+            this.SmallImageKeyBox.Name = "SmallImageKeyBox";
+            this.SmallImageKeyBox.Size = new System.Drawing.Size(227, 20);
+            this.SmallImageKeyBox.TabIndex = 31;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(23, 172);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(85, 13);
+            this.label5.TabIndex = 30;
+            this.label5.Text = "Small Image Key";
+            // 
+            // StopButton
+            // 
+            this.StopButton.Location = new System.Drawing.Point(224, 404);
+            this.StopButton.Name = "StopButton";
+            this.StopButton.Size = new System.Drawing.Size(125, 23);
+            this.StopButton.TabIndex = 29;
+            this.StopButton.Text = "Stop";
+            this.StopButton.UseVisualStyleBackColor = true;
+            this.StopButton.Click += new System.EventHandler(this.StopButton_Click);
+            // 
+            // StartButton
+            // 
+            this.StartButton.Location = new System.Drawing.Point(27, 404);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(120, 23);
+            this.StartButton.TabIndex = 27;
+            this.StartButton.Text = "Start";
+            this.StartButton.UseVisualStyleBackColor = true;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // PartyIDBox
+            // 
+            this.PartyIDBox.Location = new System.Drawing.Point(121, 266);
+            this.PartyIDBox.Name = "PartyIDBox";
+            this.PartyIDBox.Size = new System.Drawing.Size(227, 20);
+            this.PartyIDBox.TabIndex = 26;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(23, 269);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 13);
+            this.label4.TabIndex = 25;
+            this.label4.Text = "Party ID";
+            // 
+            // SmallImageTextBox
+            // 
+            this.SmallImageTextBox.Location = new System.Drawing.Point(121, 192);
+            this.SmallImageTextBox.Name = "SmallImageTextBox";
+            this.SmallImageTextBox.Size = new System.Drawing.Size(227, 20);
+            this.SmallImageTextBox.TabIndex = 24;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 195);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(88, 13);
+            this.label3.TabIndex = 23;
+            this.label3.Text = "Small Image Text";
+            // 
+            // StateBox
+            // 
+            this.StateBox.Location = new System.Drawing.Point(123, 45);
+            this.StateBox.Name = "StateBox";
+            this.StateBox.Size = new System.Drawing.Size(227, 20);
+            this.StateBox.TabIndex = 20;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 13);
+            this.label1.TabIndex = 19;
+            this.label1.Text = "State (Title)";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(690, 440);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Discord Presence Setter - by General Wrex";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.CurrentPartySize)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.MaxPartySize)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TextBox FormLog;
+        private System.Windows.Forms.TextBox MyApplicationID;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox DetailsBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox SpectateSecretBox;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox JoinSecretBox;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.NumericUpDown CurrentPartySize;
+        private System.Windows.Forms.TextBox MatchSecretBox;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.NumericUpDown MaxPartySize;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox SmallImageKeyBox;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button StopButton;
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.TextBox PartyIDBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox SmallImageTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox StateBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox LargeImageKeyBox;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.TextBox LargeImageTextBox;
+        private System.Windows.Forms.Label label13;
+    }
+}
+
---
+++ DiscordPresenceSetter/Form1.cs
@@ -0,0 +1,209 @@
+﻿using DiscordPresenceSetter.Properties;
+using System;
+using System.ComponentModel;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace DiscordPresenceSetter
+{
+    public partial class Form1 : Form
+    {
+        public DiscordRpc.RichPresence discordPresence;
+
+        public string applicationId;
+        public string optionalSteamId;
+
+        public bool isRunning = false;
+
+        private DiscordRpc.EventHandlers handlers;
+
+        private BackgroundWorker m_oWorker;
+
+        public void SaveSettings()
+        {
+            Settings.Default.ApplicationID = MyApplicationID.Text;
+            Settings.Default.State = StateBox.Text;
+            Settings.Default.Details = DetailsBox.Text;
+            Settings.Default.SmallImageKey = SmallImageKeyBox.Text;
+            Settings.Default.SmallImageText = SmallImageTextBox.Text;
+            Settings.Default.LargeImageKey = LargeImageKeyBox.Text;
+            Settings.Default.LargeImageText = LargeImageTextBox.Text;
+            Settings.Default.PartyID = PartyIDBox.Text;
+            Settings.Default.MatchSecret = MatchSecretBox.Text;
+            Settings.Default.JoinSecret = JoinSecretBox.Text;
+            Settings.Default.SpectateSecret = SpectateSecretBox.Text;
+            Settings.Default.PartySize = int.Parse(CurrentPartySize.Value.ToString());
+            Settings.Default.PartyMax = int.Parse(MaxPartySize.Value.ToString());
+
+            Settings.Default.StartTimeStamp = DateTime.Now.Ticks;
+            Settings.Default.EndTimeStamp = new TimeSpan(Settings.Default.StartTimeStamp + 5 * 60).Ticks;
+
+            Settings.Default.Save();
+        }
+
+        public void LoadSettings()
+        {
+            MyApplicationID.Text = Settings.Default.ApplicationID;
+
+            StateBox.Text = Settings.Default.State;
+            DetailsBox.Text = Settings.Default.Details;
+            SmallImageKeyBox.Text = Settings.Default.SmallImageKey;
+            SmallImageTextBox.Text = Settings.Default.SmallImageText;
+            LargeImageKeyBox.Text = Settings.Default.LargeImageKey;
+            LargeImageTextBox.Text = Settings.Default.LargeImageText;
+            PartyIDBox.Text = Settings.Default.PartyID;
+            MatchSecretBox.Text = Settings.Default.MatchSecret;
+            JoinSecretBox.Text = Settings.Default.JoinSecret;
+            SpectateSecretBox.Text = Settings.Default.SpectateSecret;
+            CurrentPartySize.Value = Settings.Default.PartySize;
+            MaxPartySize.Value = Settings.Default.PartyMax;
+        }
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            LoadSettings();
+
+            handlers = new DiscordRpc.EventHandlers();
+            handlers.readyCallback = ReadyCallback;
+            handlers.disconnectedCallback += DisconnectedCallback;
+            handlers.errorCallback += ErrorCallback;
+            handlers.joinCallback += JoinCallback;
+            handlers.spectateCallback += SpectateCallback;
+            handlers.requestCallback += RequestCallback;
+
+            m_oWorker = new BackgroundWorker();
+
+
+            m_oWorker.DoWork += delegate (object sender, DoWorkEventArgs e)
+            {
+                while (isRunning)
+                {
+                    DiscordRpc.RunCallbacks();                   
+                };
+
+                DiscordRpc.Shutdown();
+            };
+
+            m_oWorker.RunWorkerCompleted += delegate (object sender, RunWorkerCompletedEventArgs e)
+            {
+                if (e.Cancelled)
+                {
+                    Log("Task Cancelled.");
+                }
+                else if (e.Error != null)
+                {
+                    Log("Error " + e.Error.StackTrace);
+                }
+                else
+                {
+                    Log("Task Completed...");
+                }
+
+                DiscordRpc.Shutdown();
+
+                //Change the status of the buttons on the UI accordingly
+                StartButton.Enabled = true;
+                StopButton.Enabled = false;
+
+            };
+
+            m_oWorker.WorkerSupportsCancellation = true;
+
+            Log("Discord Presence Setter Log \r\nReady To Start!");
+        }
+
+        public void UpdateDiscordPresence()
+        {
+            discordPresence.state = Settings.Default.State;
+            discordPresence.details = Settings.Default.Details;
+            discordPresence.startTimestamp = Settings.Default.StartTimeStamp;
+            discordPresence.endTimestamp = Settings.Default.EndTimeStamp;
+            discordPresence.smallImageKey = Settings.Default.SmallImageKey;
+            discordPresence.smallImageText = Settings.Default.SmallImageText;
+            discordPresence.largeImageKey = Settings.Default.LargeImageKey;
+            discordPresence.largeImageText = Settings.Default.LargeImageText;
+            discordPresence.partyId = Settings.Default.PartyID;
+            discordPresence.matchSecret = Settings.Default.MatchSecret;
+            discordPresence.joinSecret = Settings.Default.JoinSecret;
+            discordPresence.spectateSecret = Settings.Default.SpectateSecret;
+            discordPresence.partySize = Settings.Default.PartySize;
+            discordPresence.partyMax = Settings.Default.PartyMax;
+            discordPresence.instance = Settings.Default.Instance;
+
+            DiscordRpc.UpdatePresence(ref discordPresence);
+        }
+
+        public void ReadyCallback()
+        {
+            Log("Discord: ready");
+        }
+
+        public void DisconnectedCallback(int errorCode, string message)
+        {
+            Log(string.Format("Discord: disconnect {0}: {1}", errorCode, message));
+        }
+
+        public void ErrorCallback(int errorCode, string message)
+        {
+            Log(string.Format("Discord: error {0}: {1}", errorCode, message));
+        }
+
+        public void JoinCallback(string secret)
+        {
+            Log(string.Format("Discord: join ({0})", secret));
+        }
+
+        public void SpectateCallback(string secret)
+        {
+            Log(string.Format("Discord: spectate ({0})", secret));
+        }
+
+        public void RequestCallback(DiscordRpc.JoinRequest request)
+        {
+            Log(string.Format("Discord: join request {0}: {1}", request.username, request.userId));
+        }
+
+        delegate void SetStringDelegate(string parameter);
+
+        public void Log(string message)
+        {
+            if (FormLog.InvokeRequired)
+                Invoke(new SetStringDelegate(Log), new object[] { message + "\r\n" });
+            else
+                FormLog.Text += message + "\r\n";
+        }
+
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            SaveSettings();
+
+            DiscordRpc.Initialize(Settings.Default.ApplicationID, ref handlers, true, "");
+
+            UpdateDiscordPresence();
+          
+            StartButton.Enabled = false;
+            StopButton.Enabled = true;
+
+            isRunning = true;
+
+            m_oWorker.RunWorkerAsync();
+           
+        }
+
+        private void StopButton_Click(object sender, EventArgs e)
+        {
+            if (m_oWorker.IsBusy)
+            {
+                isRunning = false;
+
+                m_oWorker.CancelAsync();
+            }
+
+            SaveSettings();
+            Application.Restart();
+        }
+    }
+}
